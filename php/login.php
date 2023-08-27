14fa5463fb58f952461d29b8efb23eda888bdbb7
mikebrunogit <mikealvesfa@gmail.com>
2023-08-27 16:20:34 -0300

    cadastro e login

--- php/login.php
+++ php/login.php
@@ -24,19 +24,18 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
+$login_email = $_POST["email"];
+$login_senha = $_POST["senha"];
 
-    $cadastro_nome = $_POST["nome"];
-    $cadastro_email = $_POST["email"];
-    $cadastro_senha = $_POST["senha"];
-    $cadastro_numero = $_POST["numero"];
-    
-    $sql = "INSERT INTO tab_cuidador (cuid_nome, cuid_email, cuid_senha, cuid_telefone) VALUES ('$cadastro_nome', '$cadastro_email', '$cadastro_senha', '$cadastro_numero')";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo "Cadastro bem-sucedido!";
-    } else {
-        echo "Erro de cadastro: " . $conn->error;
-    }
+$sql = "SELECT * FROM tab_cuidador WHERE cuid_email='$login_email' AND cuid_senha='$login_senha'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+    echo "Login bem-sucedido!";
+} else {
+    echo "Erro de login.";
+}
+
 
 $conn->close();
 ?>
