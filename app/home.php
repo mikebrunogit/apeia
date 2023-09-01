86b88e1918a383a04d713a1fcfb252fe61495b4e
mikebrunogit <mikealvesfa@gmail.com>
2023-09-01 18:13:56 -0300

    atualização de diretório

--- app/home.php
+++ app/home.php
@@ -6,11 +6,11 @@
     <title>Página inicial</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet"/>
 </head>
 <body>
 <?php
-include 'navbar.php'
+include "componentes/navbar.php";
 ?>
 </body>
 </html>
