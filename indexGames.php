2fe8179af6518fae6c5f412e80427c7868ccb8b2
PauIo-Soares <134661732+PauIo-Soares@users.noreply.github.com>
2023-10-20 22:13:40 -0300

    botão css

---
+++ indexGames.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Apeia | Jogos</title>
+	<link href="indexGames.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet"/>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+	<?php
+include "../componentes/navbarLogin.php"
+	?>
+	<a class="jogos" href="./Chess/chess.php">Chess</a>
+</body>
+</html>
